5490fe23f43f3c557f286b094e44fe9b569faa01
Jânio <janioofi@gmail.com>
2024-06-17 20:52:57 -0300

    Repository, service e controller criado para o usuario

--- src/main/java/br/janioofi/task/domain/dtos/UsuarioRequestDto.java
+++
@@ -1,4 +0,0 @@
-package br.janioofi.task.domain.dtos;
-
-public record UsuarioRequestDto(String usuario, String senha) {
-}
--- src/main/java/br/janioofi/task/domain/dtos/UsuarioResponseDto.java
+++
@@ -1,4 +0,0 @@
-package br.janioofi.task.domain.dtos;
-
-public record UsuarioResponseDto(Long idUsuario, String usuario, String senha) {
-}
