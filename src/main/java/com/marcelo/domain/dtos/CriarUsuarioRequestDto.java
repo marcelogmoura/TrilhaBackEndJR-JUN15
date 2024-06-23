ad3cfc1c06e802417dc9ca1a825ecb60583b4a9c
Marcelo Moura <93623867+marcelogmoura@users.noreply.github.com>
2024-06-23 20:36:22 -0300

    feat: adicionada dependencia bean validation e mapeadas validacoes no dto de criacao para usuario

--- src/main/java/com/marcelo/domain/dtos/CriarUsuarioRequestDto.java
+++ src/main/java/com/marcelo/domain/dtos/CriarUsuarioRequestDto.java
@@ -1,14 +1,25 @@
 package com.marcelo.domain.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CriarUsuarioRequestDto {
 	
+	@Size(min = 3, max = 100 , message = "Nome deve ter 3 a 100 caracteres")
+	@NotEmpty(message = "NOME não pode ser vazio.")
 	private String nome;
 	
+	@Email(message = "Informe um EMAIL válido.")
+	@NotEmpty(message = "EMAIL não pode ser vazio.")
 	private String email;
 	
+	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$",
+			message = "A senha deve conter letras maiúsculas, minúsculas, números, símbolos e pelo menos 8 caracteres.")
+	@NotEmpty(message = "SENHA não pode ser vazio.")
 	private String senha;
 
 }
