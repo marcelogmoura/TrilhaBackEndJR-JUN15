1dd12b179729c5a2155972e10ed3c0ee2d5e0679
Marcelo Moura <93623867+marcelogmoura@users.noreply.github.com>
2024-06-23 20:45:01 -0300

    feat: criada classe para fazer a criptografia dos dados

--- src/main/java/com/marcelo/domain/services/UsuarioDomainServiceImpl.java
+++ src/main/java/com/marcelo/domain/services/UsuarioDomainServiceImpl.java
@@ -1,5 +1,18 @@
 package com.marcelo.domain.services;
 
-public class UsuarioDomainServiceImpl {
+import org.springframework.stereotype.Service;
+
+import com.marcelo.domain.dtos.CriarUsuarioRequestDto;
+import com.marcelo.domain.dtos.CriarUsuarioResponseDto;
+import com.marcelo.domain.interfaces.UsuarioDomainService;
+
+@Service
+public class UsuarioDomainServiceImpl implements UsuarioDomainService{
+
+	@Override
+	public CriarUsuarioResponseDto criarUsuario(CriarUsuarioRequestDto dto) {
+		
+		return null;
+	}
 
 }
