35d99b71fe8190b35ef7b6de71e963b167cb7b91
Marcelo Moura <93623867+marcelogmoura@users.noreply.github.com>
2024-06-24 10:19:05 -0300

    fix: alterada estrategia do banco no campo idUsuario para GenerationType.UUID

--- src/main/java/com/marcelo/domain/entities/Usuario.java
+++ src/main/java/com/marcelo/domain/entities/Usuario.java
@@ -13,7 +13,7 @@ import lombok.Data;
 public class Usuario {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID idUsuario;
 	
 	private String nome;
