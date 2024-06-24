b46e5b42aacdbf9ee01282ba37bf20176078ff5e
Marcelo Moura <93623867+marcelogmoura@users.noreply.github.com>
2024-06-24 19:59:16 -0300

    feat: implementadas as consultas jpql no repositorio de tarefa

--- src/main/java/com/marcelo/infrastructure/repositories/TarefaRepository.java
+++ src/main/java/com/marcelo/infrastructure/repositories/TarefaRepository.java
@@ -1,11 +1,22 @@
 package com.marcelo.infrastructure.repositories;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.marcelo.domain.entities.Tarefa;
 
 public interface TarefaRepository extends JpaRepository<Tarefa, UUID>{
+	    
+    @Query("SELECT t FROM tb_tarefa t WHERE t.idTarefa = :tarefaId AND t.usuario.id = :usuarioId")
+    Tarefa findByIds(@Param("tarefaId") UUID tarefaId, @Param("usuarioId") UUID usuarioId);
+    
+    @Query("SELECT t FROM tb_tarefa t WHERE t.data >= :dataMin AND t.data <= :dataMax AND t.usuario.id = :pessoaId")
+    List<Tarefa> findByDatas(@Param("dataMin") Instant dataMin, @Param("dataMax") Instant dataMax, @Param("pessoaId") UUID pessoaId);
+
 
 }
