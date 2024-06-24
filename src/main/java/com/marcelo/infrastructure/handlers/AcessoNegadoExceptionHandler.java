25920342a3055c052749cf4c57447fac342c21ff
Marcelo Moura <93623867+marcelogmoura@users.noreply.github.com>
2024-06-24 12:13:53 -0300

    feat: criada classe de excecao para retorno de unauthorized, 401

---
+++ src/main/java/com/marcelo/infrastructure/handlers/AcessoNegadoExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.marcelo.infrastructure.handlers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.marcelo.domain.exceptions.AcessoNegadoException;
+
+public class AcessoNegadoExceptionHandler {
+
+	@ExceptionHandler(AcessoNegadoException.class)
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	@ResponseBody
+	public List<String> errorHandler(AcessoNegadoException e) {
+		
+		List<String> errors = new ArrayList<String>();
+		
+		errors.add(e.getMessage());
+		
+		return errors;
+		}
+}
+
+
