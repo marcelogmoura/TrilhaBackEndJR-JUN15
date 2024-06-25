bfbfc019f953f6983b36b1bb7d4198eac7567892
Marcelo Moura <93623867+marcelogmoura@users.noreply.github.com>
2024-06-24 22:25:13 -0300

    feat: classes da tarefa impl

--- src/main/java/com/marcelo/domain/services/TarefaDomainServiceImpl.java
+++ src/main/java/com/marcelo/domain/services/TarefaDomainServiceImpl.java
@@ -1,12 +1,12 @@
 package com.marcelo.domain.services;
 
 import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.marcelo.domain.dtos.ConsultarTarefaResponseDto;
@@ -25,50 +25,69 @@ public class TarefaDomainServiceImpl implements TarefaDomainService{
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
 
 	@Override
 	public ConsultarTarefaResponseDto criarTarefa(CriarTarefaRequestDto dto, UUID idUsuario) {
 		
 		Usuario usuario = usuarioRepository.findById(idUsuario).get();
 		
-		Tarefa tarefa = new Tarefa();
-		
-
-
-		
+		Tarefa tarefa = modelMapper.map(dto, Tarefa.class);
 		tarefa.setIdTarefa(UUID.randomUUID());
 		tarefa.setUsuario(usuario);
 		
 		tarefaRepository.save(tarefa);
 
-	    return null;
-	
-	
-	
+	    return modelMapper.map(tarefa, ConsultarTarefaResponseDto.class);	
 	}
+	
 
 	@Override
 	public ConsultarTarefaResponseDto editarTarefa(EditarTarefaRequestDto dto, UUID idUsuario) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Usuario usuario = usuarioRepository.findById(idUsuario).get();
+		
+		Tarefa tarefa = modelMapper.map(dto, Tarefa.class);
+		tarefa.setUsuario(usuario);
+		
+		tarefaRepository.save(tarefa);
+		
+		return modelMapper.map(tarefa, ConsultarTarefaResponseDto.class);	
 	}
+	
 
 	@Override
 	public ConsultarTarefaResponseDto excluirTarefa(UUID idTarefa, UUID idUsuario) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Tarefa tarefa  = tarefaRepository.findByIds(idTarefa, idUsuario);
+		
+		tarefaRepository.delete(tarefa);
+		
+		return modelMapper.map(tarefa, ConsultarTarefaResponseDto.class);	
 	}
+	
 
 	@Override
 	public ConsultarTarefaResponseDto obter(UUID idTarefa, UUID idUsuario) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Tarefa tarefa  = tarefaRepository.findByIds(idTarefa, idUsuario);
+		
+		return modelMapper.map(tarefa, ConsultarTarefaResponseDto.class);	
 	}
+	
 
 	@Override
 	public List<ConsultarTarefaResponseDto> consultarTarefas(Instant dataMin, Instant dataMax, UUID idUsuario) {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<Tarefa> tarefas = tarefaRepository.findByDatas(dataMin, dataMax, idUsuario);
+		
+		return tarefas.stream()
+				.map(tarefa -> modelMapper
+				.map(tarefa, ConsultarTarefaResponseDto.class))
+				.collect(Collectors.toList());
+		
 	}
 
 }
